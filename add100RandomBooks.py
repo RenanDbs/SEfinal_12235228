a7c5180c675b97f895468babf7777c592d0fc73d
NetAcad DEVASC <changeme@example.com>
2024-06-03 00:58:39 +0000

    Code now delete First 5 and Last 5 books

--- add100RandomBooks.py
+++ add100RandomBooks.py
@@ -20,6 +20,19 @@ def getAuthToken():
     else:
         raise Exception(f"Status code {r.status_code} and text {r.text}, while trying to Auth.")
 
+def delBook(id, apiKey):
+    r = requests.delete(
+        f"{APIHOST}/api/v1/books/{id}", 
+        headers = {
+            "Content-type": "application/json",
+            "X-API-Key": apiKey
+            }
+    )
+    if r.status_code == 200:
+        print(f"Book {id} deleted.")
+    else:
+        raise Exception(f"Error code {r.status_code} and text {r.text}, while trying to delete book {id}.")
+
 def addBook(book, apiKey):
     r = requests.post(
         f"{APIHOST}/api/v1/books", 
@@ -39,11 +52,14 @@ apiKey = getAuthToken()
 
 # Using the faker module, generate random "fake" books
 fake = Faker()
-for i in range(4, 29):
-    fakeTitle = fake.catch_phrase()
-    fakeAuthor = fake.name()
-    fakeISBN = fake.isbn13()
-    book = {"id":i, "title": fakeTitle, "author": fakeAuthor, "isbn": fakeISBN}
-    # add the new random "fake" book using the API
-    addBook(book, apiKey) 
-    print(i)
+for id in range (4, 9):
+    delBook(id, apiKey)
+for id in range  (24, 29):
+    delBook(id, apiKey)
+#for i in range(4, 29):
+#   fakeTitle = fake.catch_phrase()
+#  fakeAuthor = fake.name()
+# fakeISBN = fake.isbn13()
+#book = {"id":i, "title": fakeTitle, "author": fakeAuthor, "isbn": fakeISBN}
+# add the new random "fake" book using the API
+#addBook(book, apiKey) 
